37a03584daf70d4b26aec83793376d0c00596359
Cody Moffitt <codyemoffitt@gmail.com>
2015-12-03 01:05:07 -0600

    Made player taller, Added sounds to three of the Stalins

---
+++ Assets/Teleport.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleport : MonoBehaviour {
+
+	public GameObject teleportTarget;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+
+		Vector3 pos = teleportTarget.transform.position;
+		other.gameObject.transform.position = pos;
+	}
+}
